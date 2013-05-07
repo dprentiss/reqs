3f9caeed96e63eb5c06771a1f32ce3672e27e5a1
dprentiss <dprentiss@gmail.com>
2013-05-07 19:06:07 -0400

    git init

---
+++ src/test/java/com/github/dprentiss/AppTest.java
@@ -0,0 +1,29 @@
+package com.github.dprentiss;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase {
+    
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
