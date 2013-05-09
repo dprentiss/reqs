7720dcf50a4ad5e50f4a2cd4b6b5caf22ec9072a
dprentiss <dprentiss@gmail.com>
2013-05-09 14:41:06 -0400

    basic db access, auto indexing

--- src/main/java/com/github/dprentiss/ReqsDb.java
+++ src/main/java/com/github/dprentiss/ReqsDb.java
@@ -1,14 +1,24 @@
 package com.github.dprentiss;
 
+import java.util.Iterator;
+
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.cypher.javacompat.ExecutionResult;
+
 import org.neo4j.graphdb.GraphDatabaseService;
+//import org.neo4j.graphdb.index.Index;
+//import org.neo4j.graphdb.index.IndexManager;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 
 public class ReqsDb {
     private final String STORE_DIR;
     public GraphDatabaseService graphDb;
+    public ExecutionEngine cypher;
+    // public IndexManager index;
 
     private static enum RelTypes implements RelationshipType {
         IDENTIFIES, SATIFIES
@@ -16,20 +26,39 @@ public class ReqsDb {
 
     public ReqsDb(String dbPath) {
         STORE_DIR = dbPath;
-        graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(STORE_DIR);
+        graphDb = new GraphDatabaseFactory().
+            newEmbeddedDatabaseBuilder(STORE_DIR).
+            setConfig(GraphDatabaseSettings.node_keys_indexable, "name").
+            setConfig(GraphDatabaseSettings.node_auto_indexing, "true").
+            newGraphDatabase();
         registerShutdownHook(graphDb);
+        cypher = new ExecutionEngine(graphDb);
+        //index = graphDb.index();
+        //Index<Node> stakeholders = index.forNodes("name");
     }
 
-    public Node createNode() {
+    public Node getStakeholder(String name) {
+        ExecutionResult result;
+        Iterator<Node> nCol;
+        Node stakeholder = null;
+        result = cypher.execute("start n=node:node_auto_index(name = \"" + name + "\") return n");
+        nCol = result.columnAs("n");
+        if (nCol.hasNext()) {
+            stakeholder = nCol.next();
+        }
+        return stakeholder;
+    }
+
+    public void addStakeholder(String name) {
         Transaction tx = graphDb.beginTx();
-        Node newNode;
+        Node newStakeholder;
         try {
-            newNode = graphDb.createNode();
+            newStakeholder = graphDb.createNode();
+            newStakeholder.setProperty("name", name);
             tx.success();
         } finally {
             tx.finish();
         }
-        return newNode;
     }
 
     public void createTestNode() {
@@ -43,10 +72,24 @@ public class ReqsDb {
             tx.finish();
         }
     }
-    public void addStakeholder(Node stakeholder, Node concern) {
+
+    public void addIdentifes(Node stakeholder, Node concern) {
         Transaction tx = graphDb.beginTx();
         try {
             stakeholder.createRelationshipTo(concern, RelTypes.IDENTIFIES);
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+    }
+
+    public void addConcern(String title, String body) {
+        Transaction tx = graphDb.beginTx();
+        Node newNode;
+        try {
+            newNode = graphDb.createNode();
+            newNode.setProperty("title", title);
+            newNode.setProperty("body", body);
             tx.success();
         } finally {
             tx.finish();
@@ -67,7 +110,11 @@ public class ReqsDb {
     }
 
     public static void main(String[] args) {
+        String name = "test";
         ReqsDb testDb = new ReqsDb("target/testDb");
-        testDb.createTestNode();
+        testDb.addStakeholder("Mark Austin");
+        testDb.addStakeholder("David Prentiss");
+        testDb.addConcern("Student wants a good grade", "The student wants to recieve a grade of at least A- for the projct.");
+        System.out.println(testDb.getStakeholder("David Prentiss").getProperty("name"));
     }
 }
--- src/test/java/com/github/dprentiss/ReqsDbTest.java
+++ src/test/java/com/github/dprentiss/ReqsDbTest.java
@@ -4,11 +4,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Transaction;
-
 public class ReqsDbTest extends TestCase {
-    private ReqsDb testDb = new ReqsDb("target/testDb");
+    protected ReqsDb testDb = new ReqsDb("target/testDb");
 
     public ReqsDbTest(String testName) {
         super(testName);
@@ -21,16 +18,6 @@ public class ReqsDbTest extends TestCase {
     public void testDbService() {
         assertNotNull(testDb);
     }
-    
-    /*
-    public void testAddStakeholder() {
-       Node stakeholder = testDb.createNode();
-       stakeholder.setProperty("name", "Mark Austin");
-       Node concern = testDb.createNode();
-       stakeholder.setProperty("concern", "The system must make use of graph-based data structures");
-       testDb.addStakeholder(stakeholder, concern);
-    }
-    */
 
     public void tearDown() {
         testDb.shutDown();
